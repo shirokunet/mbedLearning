29662d30a6e07cccad4a2c821fe55c00f2991218
shirokunet <shirokunet@gmail.com>
2018-09-21 23:15:32 -0700

    complete segment led

---
+++ SegmentLED/README.md
@@ -0,0 +1,14 @@
+## Photos
+![overview1](https://github.com/shirokunet/mbedLearning/raw/master/images/IMG_20180921_213822.jpg)
+![overview2](https://github.com/shirokunet/mbedLearning/raw/master/images/IMG_20180921_213856.jpg)
+![overview3](https://github.com/shirokunet/mbedLearning/raw/master/images/IMG_20180921_213929.jpg)
+![overview4](https://github.com/shirokunet/mbedLearning/raw/master/images/IMG_20180921_223442.jpg)
+
+## Movies
+[mbedLearning_SegmentLED_Terminal - YouTube](https://www.youtube.com/watch?v=Roh8OIbPQPA)
+
+![terminal](https://github.com/shirokunet/mbedLearning/raw/master/images/SegmentLED_Terminal.gif)
+
+[mbedLearning_SegmentLED - YouTube](https://www.youtube.com/watch?v=SXzEZ_PlpzI)
+
+![test](https://github.com/shirokunet/mbedLearning/raw/master/images/SegmentLED.gif)
--- SegmentLED/main.cpp
+++ SegmentLED/main.cpp
@@ -1,39 +1,41 @@
 #include "mbed.h"
 
-#define DIGIT_ALL			0b100000001000000010000010
-#define DIGIT_NUM_ALL		0b1000001110000001101000001
-#define DIGIT_DP			0b0000000000000000001000000
+#define DIGIT_ALL	0b100000001000000010000010
+#define DIGIT_NALL	0b1000001110000001101000001
+#define DIGIT_DP	0b0000000000000000001000000
 
 const int digit_mask[4] = {
-							0b100000000000000000000000,
-							0b000000001000000000000000,
-							0b000000000000000000000010,
-							0b000000000000000010000000
-						};
+	0b100000000000000000000000,
+	0b000000001000000000000000,
+	0b000000000000000000000010,
+	0b000000000000000010000000
+};
 
 const int digit_num[10] = {
-							0b0000001110000001100000001,
-							0b0000000110000000000000000,
-							0b1000000100000001100000001,
-							0b1000000110000000100000001,
-							0b1000001110000000000000000,
-							0b1000001010000000100000001,
-							0b1000001010000001100000001,
-							0b0000001110000000000000001,
-							0b1000001110000001100000001,
-							0b1000001110000000000000001
-						};
+	0b0000001110000001100000001,
+	0b0000000110000000000000000,
+	0b1000000100000001100000001,
+	0b1000000110000000100000001,
+	0b1000001110000000000000000,
+	0b1000001010000000100000001,
+	0b1000001010000001100000001,
+	0b0000001110000000000000001,
+	0b1000001110000001100000001,
+	0b1000001110000000000000001
+};
 
 PortOut digit_port(Port0, DIGIT_ALL);
-PortOut digitnum_port(Port0, DIGIT_NUM_ALL);
+PortOut digitnum_port(Port0, DIGIT_NALL);
 
 Serial pc(USBTX, USBRX, 115200);
 
 int main() {
-	int count = 0;
+	Timer t;
+	t.start();
+	int now = 0;
 	int digit[4] = {0};
 	while(1) {
-		count += 1;
+		now = int(t.read()*10.0);
 		for (int i=sizeof(digit)/sizeof(digit[0])-1; i>=0; i--){
 			/* pow function */
 			int denominator = 1;
@@ -42,15 +44,19 @@ int main() {
 			}
 
 			/* store digit number */
-			digit[i] = (count/denominator)%10;
+			digit[i] = (now/denominator)%10;
 
 			/* blink LED */
 			digit_port = digit_mask[i];
-			digitnum_port = digit_num[digit[i]];
+			if (i==1)
+				digitnum_port = digit_num[digit[i]] | DIGIT_DP;
+			else
+				digitnum_port = digit_num[digit[i]];
+			wait(0.001);
+			digit_port = 0;
 
 			pc.printf("%d,", digit[i]);
 		}
-		pc.printf(": %d\r\n", count);
-		wait(0.5);
+		pc.printf(": %d\r\n", now);
 	}
 }

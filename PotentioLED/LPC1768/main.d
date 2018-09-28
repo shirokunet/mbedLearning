5343d21652ad8f320da39e7faf42fae7e743c3c6
shirokunet <shirokunet@gmail.com>
2018-09-28 15:03:12 -0700

    add PotentioLED

---
+++ PotentioLED/LPC1768/main.d
@@ -0,0 +1,353 @@
+LPC1768/main.o: main.cpp ../../src/mbed_config.h \
+ ../../external/mbed-os/mbed.h \
+ ../../external/mbed-os/events/mbed_events.h \
+ ../../external/mbed-os/events/equeue/equeue.h \
+ ../../external/mbed-os/events/equeue/equeue_platform.h \
+ ../../external/mbed-os/events/EventQueue.h \
+ ../../external/mbed-os/platform/Callback.h \
+ ../../external/mbed-os/platform/mbed_assert.h \
+ ../../external/mbed-os/platform/mbed_preprocessor.h \
+ ../../external/mbed-os/platform/mbed_toolchain.h \
+ ../../external/mbed-os/events/Event.h \
+ ../../external/mbed-os/features/filesystem/mbed_filesystem.h \
+ ../../external/mbed-os/platform/platform.h \
+ ../../external/mbed-os/platform/mbed_retarget.h \
+ ../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/TARGET_MBED_LPC1768/device.h \
+ ../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/objects.h \
+ ../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/device/cmsis.h \
+ ../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/device/LPC17xx.h \
+ ../../external/mbed-os/cmsis/core_cm3.h \
+ ../../external/mbed-os/cmsis/core_cmInstr.h \
+ ../../external/mbed-os/cmsis/core_cmFunc.h \
+ ../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/device/system_LPC17xx.h \
+ ../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/device/cmsis_nvic.h \
+ ../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/device/cmsis.h \
+ ../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/PortNames.h \
+ ../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/PeripheralNames.h \
+ ../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/TARGET_MBED_LPC1768/PinNames.h \
+ ../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/gpio_object.h \
+ ../../external/mbed-os/platform/mbed_assert.h \
+ ../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/PeripheralNames.h \
+ ../../external/mbed-os/features/filesystem/FileSystem.h \
+ ../../external/mbed-os/drivers/FileBase.h \
+ ../../external/mbed-os/platform/SingletonPtr.h \
+ ../../external/mbed-os/platform/PlatformMutex.h \
+ ../../external/mbed-os/features/filesystem/bd/BlockDevice.h \
+ ../../external/mbed-os/features/filesystem/File.h \
+ ../../external/mbed-os/drivers/FileLike.h \
+ ../../external/mbed-os/features/filesystem/Dir.h \
+ ../../external/mbed-os/features/filesystem/bd/BlockDevice.h \
+ ../../external/mbed-os/features/filesystem/bd/ChainingBlockDevice.h \
+ ../../external/mbed-os/features/filesystem/bd/BlockDevice.h \
+ ../../external/mbed-os/features/filesystem/bd/SlicingBlockDevice.h \
+ ../../external/mbed-os/features/filesystem/bd/HeapBlockDevice.h \
+ ../../external/mbed-os/platform/mbed_toolchain.h \
+ ../../external/mbed-os/platform/platform.h \
+ ../../external/mbed-os/platform/mbed_application.h \
+ ../../external/mbed-os/platform/mbed_error.h \
+ ../../external/mbed-os/platform/mbed_interface.h \
+ ../../external/mbed-os/platform/mbed_assert.h \
+ ../../external/mbed-os/drivers/DigitalIn.h \
+ ../../external/mbed-os/hal/gpio_api.h \
+ ../../external/mbed-os/platform/mbed_critical.h \
+ ../../external/mbed-os/drivers/DigitalOut.h \
+ ../../external/mbed-os/drivers/DigitalInOut.h \
+ ../../external/mbed-os/drivers/BusIn.h \
+ ../../external/mbed-os/drivers/DigitalIn.h \
+ ../../external/mbed-os/drivers/BusOut.h \
+ ../../external/mbed-os/drivers/DigitalOut.h \
+ ../../external/mbed-os/drivers/BusInOut.h \
+ ../../external/mbed-os/drivers/DigitalInOut.h \
+ ../../external/mbed-os/drivers/PortIn.h \
+ ../../external/mbed-os/hal/port_api.h \
+ ../../external/mbed-os/drivers/PortInOut.h \
+ ../../external/mbed-os/drivers/PortOut.h \
+ ../../external/mbed-os/drivers/AnalogIn.h \
+ ../../external/mbed-os/hal/analogin_api.h \
+ ../../external/mbed-os/drivers/AnalogOut.h \
+ ../../external/mbed-os/hal/analogout_api.h \
+ ../../external/mbed-os/drivers/PwmOut.h \
+ ../../external/mbed-os/hal/pwmout_api.h \
+ ../../external/mbed-os/drivers/Serial.h \
+ ../../external/mbed-os/drivers/Stream.h \
+ ../../external/mbed-os/drivers/FileHandle.h \
+ ../../external/mbed-os/drivers/SerialBase.h \
+ ../../external/mbed-os/platform/Callback.h \
+ ../../external/mbed-os/hal/serial_api.h \
+ ../../external/mbed-os/hal/buffer.h ../../external/mbed-os/hal/dma_api.h \
+ ../../external/mbed-os/platform/mbed_toolchain.h \
+ ../../external/mbed-os/platform/PlatformMutex.h \
+ ../../external/mbed-os/drivers/SPI.h \
+ ../../external/mbed-os/hal/spi_api.h \
+ ../../external/mbed-os/drivers/SPISlave.h \
+ ../../external/mbed-os/drivers/I2C.h \
+ ../../external/mbed-os/hal/i2c_api.h \
+ ../../external/mbed-os/drivers/I2CSlave.h \
+ ../../external/mbed-os/drivers/Ethernet.h \
+ ../../external/mbed-os/drivers/CAN.h \
+ ../../external/mbed-os/hal/can_api.h \
+ ../../external/mbed-os/hal/can_helper.h \
+ ../../external/mbed-os/drivers/RawSerial.h \
+ ../../external/mbed-os/drivers/SerialBase.h \
+ ../../external/mbed-os/hal/serial_api.h \
+ ../../external/mbed-os/drivers/FlashIAP.h \
+ ../../external/mbed-os/drivers/Timer.h \
+ ../../external/mbed-os/hal/ticker_api.h \
+ ../../external/mbed-os/drivers/Ticker.h \
+ ../../external/mbed-os/drivers/TimerEvent.h \
+ ../../external/mbed-os/hal/us_ticker_api.h \
+ ../../external/mbed-os/drivers/Timeout.h \
+ ../../external/mbed-os/drivers/Ticker.h \
+ ../../external/mbed-os/drivers/LowPowerTimeout.h \
+ ../../external/mbed-os/drivers/LowPowerTicker.h \
+ ../../external/mbed-os/drivers/LowPowerTimer.h \
+ ../../external/mbed-os/drivers/Timer.h \
+ ../../external/mbed-os/drivers/LocalFileSystem.h \
+ ../../external/mbed-os/drivers/FileSystemLike.h \
+ ../../external/mbed-os/drivers/DirHandle.h \
+ ../../external/mbed-os/drivers/FileHandle.h \
+ ../../external/mbed-os/drivers/InterruptIn.h \
+ ../../external/mbed-os/hal/gpio_irq_api.h \
+ ../../external/mbed-os/platform/mbed_wait_api.h \
+ ../../external/mbed-os/hal/sleep_api.h \
+ ../../external/mbed-os/platform/mbed_sleep.h \
+ ../../external/mbed-os/hal/sleep_api.h \
+ ../../external/mbed-os/platform/mbed_rtc_time.h \
+ ../../external/mbed-os/platform/Callback.h \
+ ../../external/mbed-os/platform/FunctionPointer.h
+
+../../src/mbed_config.h:
+
+../../external/mbed-os/mbed.h:
+
+../../external/mbed-os/events/mbed_events.h:
+
+../../external/mbed-os/events/equeue/equeue.h:
+
+../../external/mbed-os/events/equeue/equeue_platform.h:
+
+../../external/mbed-os/events/EventQueue.h:
+
+../../external/mbed-os/platform/Callback.h:
+
+../../external/mbed-os/platform/mbed_assert.h:
+
+../../external/mbed-os/platform/mbed_preprocessor.h:
+
+../../external/mbed-os/platform/mbed_toolchain.h:
+
+../../external/mbed-os/events/Event.h:
+
+../../external/mbed-os/features/filesystem/mbed_filesystem.h:
+
+../../external/mbed-os/platform/platform.h:
+
+../../external/mbed-os/platform/mbed_retarget.h:
+
+../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/TARGET_MBED_LPC1768/device.h:
+
+../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/objects.h:
+
+../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/device/cmsis.h:
+
+../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/device/LPC17xx.h:
+
+../../external/mbed-os/cmsis/core_cm3.h:
+
+../../external/mbed-os/cmsis/core_cmInstr.h:
+
+../../external/mbed-os/cmsis/core_cmFunc.h:
+
+../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/device/system_LPC17xx.h:
+
+../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/device/cmsis_nvic.h:
+
+../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/device/cmsis.h:
+
+../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/PortNames.h:
+
+../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/PeripheralNames.h:
+
+../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/TARGET_MBED_LPC1768/PinNames.h:
+
+../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/gpio_object.h:
+
+../../external/mbed-os/platform/mbed_assert.h:
+
+../../external/mbed-os/targets/TARGET_NXP/TARGET_LPC176X/PeripheralNames.h:
+
+../../external/mbed-os/features/filesystem/FileSystem.h:
+
+../../external/mbed-os/drivers/FileBase.h:
+
+../../external/mbed-os/platform/SingletonPtr.h:
+
+../../external/mbed-os/platform/PlatformMutex.h:
+
+../../external/mbed-os/features/filesystem/bd/BlockDevice.h:
+
+../../external/mbed-os/features/filesystem/File.h:
+
+../../external/mbed-os/drivers/FileLike.h:
+
+../../external/mbed-os/features/filesystem/Dir.h:
+
+../../external/mbed-os/features/filesystem/bd/BlockDevice.h:
+
+../../external/mbed-os/features/filesystem/bd/ChainingBlockDevice.h:
+
+../../external/mbed-os/features/filesystem/bd/BlockDevice.h:
+
+../../external/mbed-os/features/filesystem/bd/SlicingBlockDevice.h:
+
+../../external/mbed-os/features/filesystem/bd/HeapBlockDevice.h:
+
+../../external/mbed-os/platform/mbed_toolchain.h:
+
+../../external/mbed-os/platform/platform.h:
+
+../../external/mbed-os/platform/mbed_application.h:
+
+../../external/mbed-os/platform/mbed_error.h:
+
+../../external/mbed-os/platform/mbed_interface.h:
+
+../../external/mbed-os/platform/mbed_assert.h:
+
+../../external/mbed-os/drivers/DigitalIn.h:
+
+../../external/mbed-os/hal/gpio_api.h:
+
+../../external/mbed-os/platform/mbed_critical.h:
+
+../../external/mbed-os/drivers/DigitalOut.h:
+
+../../external/mbed-os/drivers/DigitalInOut.h:
+
+../../external/mbed-os/drivers/BusIn.h:
+
+../../external/mbed-os/drivers/DigitalIn.h:
+
+../../external/mbed-os/drivers/BusOut.h:
+
+../../external/mbed-os/drivers/DigitalOut.h:
+
+../../external/mbed-os/drivers/BusInOut.h:
+
+../../external/mbed-os/drivers/DigitalInOut.h:
+
+../../external/mbed-os/drivers/PortIn.h:
+
+../../external/mbed-os/hal/port_api.h:
+
+../../external/mbed-os/drivers/PortInOut.h:
+
+../../external/mbed-os/drivers/PortOut.h:
+
+../../external/mbed-os/drivers/AnalogIn.h:
+
+../../external/mbed-os/hal/analogin_api.h:
+
+../../external/mbed-os/drivers/AnalogOut.h:
+
+../../external/mbed-os/hal/analogout_api.h:
+
+../../external/mbed-os/drivers/PwmOut.h:
+
+../../external/mbed-os/hal/pwmout_api.h:
+
+../../external/mbed-os/drivers/Serial.h:
+
+../../external/mbed-os/drivers/Stream.h:
+
+../../external/mbed-os/drivers/FileHandle.h:
+
+../../external/mbed-os/drivers/SerialBase.h:
+
+../../external/mbed-os/platform/Callback.h:
+
+../../external/mbed-os/hal/serial_api.h:
+
+../../external/mbed-os/hal/buffer.h:
+
+../../external/mbed-os/hal/dma_api.h:
+
+../../external/mbed-os/platform/mbed_toolchain.h:
+
+../../external/mbed-os/platform/PlatformMutex.h:
+
+../../external/mbed-os/drivers/SPI.h:
+
+../../external/mbed-os/hal/spi_api.h:
+
+../../external/mbed-os/drivers/SPISlave.h:
+
+../../external/mbed-os/drivers/I2C.h:
+
+../../external/mbed-os/hal/i2c_api.h:
+
+../../external/mbed-os/drivers/I2CSlave.h:
+
+../../external/mbed-os/drivers/Ethernet.h:
+
+../../external/mbed-os/drivers/CAN.h:
+
+../../external/mbed-os/hal/can_api.h:
+
+../../external/mbed-os/hal/can_helper.h:
+
+../../external/mbed-os/drivers/RawSerial.h:
+
+../../external/mbed-os/drivers/SerialBase.h:
+
+../../external/mbed-os/hal/serial_api.h:
+
+../../external/mbed-os/drivers/FlashIAP.h:
+
+../../external/mbed-os/drivers/Timer.h:
+
+../../external/mbed-os/hal/ticker_api.h:
+
+../../external/mbed-os/drivers/Ticker.h:
+
+../../external/mbed-os/drivers/TimerEvent.h:
+
+../../external/mbed-os/hal/us_ticker_api.h:
+
+../../external/mbed-os/drivers/Timeout.h:
+
+../../external/mbed-os/drivers/Ticker.h:
+
+../../external/mbed-os/drivers/LowPowerTimeout.h:
+
+../../external/mbed-os/drivers/LowPowerTicker.h:
+
+../../external/mbed-os/drivers/LowPowerTimer.h:
+
+../../external/mbed-os/drivers/Timer.h:
+
+../../external/mbed-os/drivers/LocalFileSystem.h:
+
+../../external/mbed-os/drivers/FileSystemLike.h:
+
+../../external/mbed-os/drivers/DirHandle.h:
+
+../../external/mbed-os/drivers/FileHandle.h:
+
+../../external/mbed-os/drivers/InterruptIn.h:
+
+../../external/mbed-os/hal/gpio_irq_api.h:
+
+../../external/mbed-os/platform/mbed_wait_api.h:
+
+../../external/mbed-os/hal/sleep_api.h:
+
+../../external/mbed-os/platform/mbed_sleep.h:
+
+../../external/mbed-os/hal/sleep_api.h:
+
+../../external/mbed-os/platform/mbed_rtc_time.h:
+
+../../external/mbed-os/platform/Callback.h:
+
+../../external/mbed-os/platform/FunctionPointer.h:
---
+++ PotentioLED/README.md
@@ -0,0 +1,8 @@
+## Photos
+![overview](https://github.com/shirokunet/mbedLearning/raw/master/images/IMG_20180928_145225.jpg)
+
+## Movies
+[mbed Learning PotentioLED - YouTube](https://www.youtube.com/watch?v=TmPrUbAwzkA)
+
+![Test](https://github.com/shirokunet/mbedLearning/raw/master/images/PotentioLED.gif)
+

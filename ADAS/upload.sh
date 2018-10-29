b02247d295775388e29cb3ad4dd672d1e74f71e2
shirokunet <shirokunet@gmail.com>
2018-10-29 11:03:53 -0700

    delete

--- ADAS/upload.sh
+++
@@ -1,1 +0,0 @@
-mbed compile -t GCC_ARM -m NUCLEO_F446RE --source ../IzanagiServo --source ../mbed-os --build ../BUILD/IzanagiServo ; cp -f ../BUILD/IzanagiServo/IzanagiServo.bin /media/reyes/NODE_F446*
